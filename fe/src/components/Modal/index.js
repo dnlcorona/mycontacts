1b06f8bbbee170482b9687f77eedf7a66cd1e7aa
dnlcorona <danielcorona313@gmail.com>
2023-05-02 08:47:09 -0300

    feat: danger style prop to modal

--- fe/src/components/Modal/index.js
+++ fe/src/components/Modal/index.js
@@ -1,11 +1,13 @@
+import PropTypes from 'prop-types'
+
 import { Overlay, Container, Footer } from './styles'
 
 import Button from '../Button'
 
-export default function Modal() {
+export default function Modal({ danger }) {
 	return (
 		<Overlay>
-			<Container>
+			<Container danger={danger}>
 				<h1>Título do Modal</h1>
 				<p>
           Corpo do Modal
@@ -14,11 +16,19 @@ export default function Modal() {
 					<button type="button" className="cancel-button">
             Cancelar
 					</button>
-					<Button type="button">
+					<Button type="button" danger={danger}>
             Deletar
 					</Button>
 				</Footer>
 			</Container>
 		</Overlay>
 	)
+}
+
+Modal.propTypes = {
+	danger: PropTypes.bool,
+}
+
+Modal. defaultProps = {
+	danger: false,
 }
