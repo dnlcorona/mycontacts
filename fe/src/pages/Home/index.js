38d36f7e31511e72fd9cb9c0d8de248203f460b6
dnlcorona <danielcorona313@gmail.com>
2023-05-24 17:18:46 -0300

    feat: add .map to render the api contacts

--- fe/src/pages/Home/index.js
+++ fe/src/pages/Home/index.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 
-import { Container, InputSearchContainer, Header, ListContainer, Card } from './styles'
+import { Container, InputSearchContainer, Header, ListHeader, Card } from './styles'
 
 import arrow from '../../assets/images/icons/arrow.svg'
 import edit from '../../assets/images/icons/edit.svg'
@@ -9,19 +9,24 @@ import trash from '../../assets/images/icons/trash.svg'
 
 export default function Home() {
 	const [contacts, setContacts] = useState([])
+	const [orderBy, setOrderBy] = useState('asc')
 
 	useEffect(() => {
-		fetch('http://localhost:3333/contacts')
+		fetch(`http://localhost:3333/contacts?orderBy=${orderBy}`)
 			.then(async (response) => {
 				const json = await response.json()
 				setContacts(json)
 			})
 			.catch((error) => {
-				console.log('error', error)
+				console.log('erro', error)
 			})
-	}, [])
+	}, [orderBy])
 
-
+	function handleToggleOrderBy() {
+		setOrderBy(
+			(prevState) => (prevState === 'asc' ? 'desc' : 'asc')
+		)
+	}
 
 	return (
 		<Container>
@@ -30,30 +35,35 @@ export default function Home() {
 			</InputSearchContainer>
 
 			<Header>
-				<strong>3 contatos</strong>
+				<strong>
+					{contacts.length}
+					{contacts.length === 1 ? ' contato' : ' contatos'}
+				</strong>
 				<Link to="/new">Novo contato</Link>
 			</Header>
 
-			<ListContainer>
-				<header>
-					<button type="button">
-						<span>Nome</span>
-						<img src={arrow} alt="Seta" />
-					</button>
-				</header>
+			<ListHeader orderBy={orderBy}>
+				<button type="button" onClick={handleToggleOrderBy}>
+					<span>Nome</span>
+					<img src={arrow} alt="Seta" />
+				</button>
+			</ListHeader>
 
-				<Card>
+			{contacts.map((contact) => (
+				<Card key={contact.id}>
 					<div className="info">
 						<div className="contact-name">
-							<strong>Daniel Corona</strong>
-							<small>instagram</small>
+							<strong>{contact.name}</strong>
+							{contact.category_name && (
+								<small>{contact.category_name}</small>
+							)}
 						</div>
-						<span>danielcorona313@gmail.com</span>
-						<span>(27) 99999-9999</span>
+						<span>{contact.email}</span>
+						<span>{contact.phone}</span>
 					</div>
 
 					<div className="actions">
-						<Link to="/edit/123">
+						<Link to={`/edit/${contact.id}`}>
 							<img src={edit} alt="Editar" />
 						</Link>
 						<button type="button">
@@ -61,7 +71,7 @@ export default function Home() {
 						</button>
 					</div>
 				</Card>
-			</ListContainer>
+			))}
 		</Container>
 	)
 }
