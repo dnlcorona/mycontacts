a0a89757eea5ba2f09b7354a10594bb545e6f55a
dnlcorona <danielcorona313@gmail.com>
2023-06-02 15:49:01 -0300

    feat: add variants to toast

---
+++ fe/src/components/Toast/ToastContainer/index.js
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+
+import { Container } from './styles'
+
+import ToastMessage from '../ToastMessage'
+
+export default function ToastContainer() {
+	const [messages, setMessages] = useState([
+		{ id: Math.random(), type: 'default', text: 'Default text' },
+		{ id: Math.random(), type: 'danger', text: 'Danger text' },
+		{ id: Math.random(), type: 'success', text: 'Success text' },
+	])
+
+	return(
+		<Container>
+			{messages.map((message) => (
+				<ToastMessage
+					key={message.id}
+					type={message.type}
+					text={message.text}
+				/>
+			))}
+		</Container>
+	)
+}
