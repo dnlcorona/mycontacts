d10e047714ef09cd2a7849256604a4afdf948903
dnlcorona <danielcorona313@gmail.com>
2023-06-29 14:07:36 -0300

    refac: create custom hooks to separe UI and logic

--- fe/src/pages/Home/index.js
+++ fe/src/pages/Home/index.js
@@ -1,4 +1,3 @@
-import { useEffect, useState, useMemo, useCallback } from 'react'
 import { Link } from 'react-router-dom'
 
 import { Container, InputSearchContainer, Header, ListHeader, Card, ErrorContainer, EmptyListContainer, SearchNotFoundContainer } from './styles'
@@ -14,94 +13,26 @@ import Loader from '../../components/Loader'
 import Button from '../../components/Button'
 import Modal from '../../components/Modal'
 
-import ContactsService from '../../services/ContactsService'
-
-import toast from '../../utils/toast'
+import useHome from './useHome'
 
 export default function Home() {
-	const [contacts, setContacts] = useState([])
-	const [orderBy, setOrderBy] = useState('asc')
-	const [searchTerm, setSearchTerm] = useState('')
-	const [isLoading, setIsLoading] = useState(true)
-	const [hasError, setHasError] = useState(false)
-	const [isDeleteModalVisible, setIsDeleteModalVisible] = useState(false)
-	const [contactBeingDeleted, setContactBeingDeleted] = useState(null)
-	const [isLoadingDelete, setIsLoadingDelete] = useState(false)
-
-	const filteredContacts = useMemo(() => {
-		return contacts.filter((contact) => (
-			contact.name.toLowerCase().includes(searchTerm.toLowerCase())
-		))
-	}, [contacts, searchTerm])
-
-	const loadContacts = useCallback(async () => {
-		try {
-			setIsLoading(true)
-
-			const contactsList = await ContactsService.listContacts(orderBy)
-
-			setHasError(false)
-			setContacts(contactsList)
-		} catch {
-			setHasError(true)
-		} finally {
-			setIsLoading(false)
-		}
-	}, [orderBy])
-
-	useEffect(() => {
-		loadContacts()
-	}, [loadContacts])
-
-	function handleToggleOrderBy() {
-		setOrderBy(
-			(prevState) => (prevState === 'asc' ? 'desc' : 'asc')
-		)
-	}
-
-	function handleChangeSearchTerm(event) {
-		setSearchTerm(event.target.value)
-	}
-
-	function handleTryAgain() {
-		loadContacts()
-	}
-
-	function handleDeleteContact(contact) {
-		setContactBeingDeleted(contact)
-		setIsDeleteModalVisible(true)
-	}
-
-	function handleCloseDeleteModal() {
-		setIsDeleteModalVisible(false)
-		setContactBeingDeleted(null)
-	}
-
-	async function handleConfirmDeleteContact() {
-		try {
-			setIsLoadingDelete(true)
-
-			await ContactsService.deleteContact(contactBeingDeleted.id)
-
-			setContacts(prevState => prevState.filter(
-				(contact) => contact.id !== contactBeingDeleted.id
-			))
-
-			handleCloseDeleteModal()
-
-			toast({
-				type: 'success',
-				text: 'Contato deletado com sucesso!'
-			})
-		} catch {
-			toast({
-				type: 'danger',
-				text: 'Ocorreu um erro ao deletar o contato!'
-			})
-		} finally {
-			setIsLoadingDelete(false)
-		}
-	}
+	const {
+		isLoading,
+		contactBeingDeleted,
+		isDeleteModalVisible,
+		isLoadingDelete,
+		handleCloseDeleteModal,
+		handleConfirmDeleteContact,
+		contacts,
+		handleChangeSearchTerm,
+		hasError,
+		filteredContacts,
+		handleTryAgain,
+		searchTerm,
+		orderBy,
+		handleToggleOrderBy,
+		handleDeleteContact,
+	} = useHome()
 
 	return (
 		<Container>
